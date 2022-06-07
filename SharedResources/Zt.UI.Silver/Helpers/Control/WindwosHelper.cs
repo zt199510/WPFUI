3f0d551c9e75a042c3bf15a0163617ddab349fd0
zkzx <zkzx@DESKTOP-F5CP57V>
2022-06-07 16:36:18 +0800

    更新UI

---
+++ SharedResources/Zt.UI.Silver/Helpers/Control/WindwosHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Zt.UI.Silver
+{
+    public class WindwosHelper : Window
+    {
+
+        static WindwosHelper()
+        {
+            StyleProperty.OverrideMetadata(typeof(WindwosHelper), new FrameworkPropertyMetadata(Application.Current.FindResource("ZTWindowsStyle")));
+        }
+    }
+}
